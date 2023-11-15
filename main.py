4d8e7cc682bb90ea6c6263e5e052ce42bf53ba03
ivan2k4 <126463137+ivan2k4@users.noreply.github.com>
2023-11-15 01:09:55 +0100

    yepa

--- main.py
+++ main.py
@@ -11,12 +11,31 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 import magic 
+import matplotlib.pyplot as plt
 
 def file_extension(file):
+    """
+    Obtiene la extensión de un archivo a partir de su nombre.
+
+    Parameters:
+    - file (str): Nombre del archivo.
+
+    Returns:
+    - str: Extensión del archivo.
+    """
     L = file.split('.')
     return L[-1]
 
 def interpretar_r_cuadrado(r_cuadrado):
+    """
+    Interpreta el valor de R-cuadrado y devuelve el color y la interpretación correspondientes.
+
+    Parameters:
+    - r_cuadrado (float): Valor de R-cuadrado.
+
+    Returns:
+    - Tuple: (str) Color para visualización, (str) Interpretación del R-cuadrado.
+    """
     if 0.8 <= r_cuadrado <= 1:
         color = 'green'
         interpretacion = 'Ajuste óptimo, el modelo explica a la perfección la relación de las variables'
@@ -36,6 +55,12 @@ def interpretar_r_cuadrado(r_cuadrado):
     return color, interpretacion
 
 def mostrar_resultados(modelo):
+    """
+    Muestra los resultados de un modelo de regresión lineal, incluido el R-cuadrado.
+
+    Parameters:
+    - modelo: Modelo de regresión lineal ajustado.
+    """
     resultados = modelo.summary()
     resultados_str = str(resultados)
     resultados_list = resultados_str.split('\n')
@@ -55,9 +80,7 @@ def mostrar_resultados(modelo):
         [sg.Multiline(resultados_str, size=(80, 20), font=('Courier New', 10))],
         [sg.Text(f'Bondad del ajuste: {r_cuadrado:.4f}', font=('Helvetica', 14), text_color=color)],
         [sg.Text(f'Interpretación: {interpretacion}', font=('Helvetica', 12))],
-        [sg.Button('OK', size=(10, 2), pad=((20, 0), 3), button_color=('white', 'green')),sg.SaveAs('Guardar Modelo', size=(10, 2), pad=((20, 0), 3), button_color=('white', 'blue'),file_types=(('FARLOPA', '.farlopa'),),default_extension=str('.farlopa'),key='--FILE--' )]
-
-    ]
+        [sg.Button('OK', size=(10, 2), pad=((20, 0), 3), button_color=('white', 'green')),sg.SaveAs('Guardar Modelo', size=(10, 2), pad=((20, 0), 3), button_color=('white', 'blue'),file_types=(('FARLOPA', '.farlopa'),),default_extension=str('.farlopa'),key='--FILE--' )]]
 
     window = sg.Window('Resultados', layout, finalize=True)
 
@@ -74,6 +97,13 @@ def mostrar_resultados(modelo):
 
 
 def regression_interface(dfs, selected_file):
+    """
+    Interfaz gráfica para realizar una regresión lineal.
+
+    Parameters:
+    - dfs (dict): Diccionario que contiene los DataFrames cargados.
+    - selected_file (str): Nombre del archivo seleccionado.
+    """
     df = dfs[selected_file]
     columnas = list(df.columns)
 
@@ -86,8 +116,7 @@ def regression_interface(dfs, selected_file):
         [sg.Text('Seleccione la variable a predecir:', font=('Helvetica', 12), size=(25, 1)),
          sg.InputCombo(values=columnas, key='predecir')],
         [sg.Button('Realizar Regresión Lineal', size=(20, 2), button_color=('white', 'green')),
-         sg.Button('Salir', size=(20, 2), button_color=('white', 'red'))],
-    ]
+         sg.Button('Salir', size=(20, 2), button_color=('white', 'red'))],]
 
     window = sg.Window('Regresión Lineal', layout, finalize=True)
 
@@ -118,23 +147,28 @@ def regression_interface(dfs, selected_file):
 
     window.close()
 
+    
+
 def interface(dfs:dict):
+    """
+    Interfaz principal que permite cargar archivos, realizar regresiones lineales y gestionar modelos.
+
+    Parameters:
+    - dfs (dict): Diccionario que contiene los DataFrames cargados.
+    """
     layout0 = [
         [sg.Button('Cargar archivo')],
         [sg.Button('Mostrar modelos')],
-        [sg.Button('Cargar modelo')]
-    ]
+        [sg.Button('Cargar modelo')]]
     
     layout1 = [
         [sg.Text('Selecciona un archivo')],
-        [sg.InputText(key='-Archivo-'), sg.FileBrowse(file_types=(("All Files", "*.*"),))],
-        [sg.Button('Cargar Archivo'), sg.Button('Realizar Regresión Lineal'), sg.Button('Salir')]
-    ]
+        [sg.InputText(key='-Archivo-'),sg.FileBrowse(file_types=(("All Files", "*.*"),))],
+        [sg.Button('Cargar Archivo'), sg.Button('Realizar Regresión Lineal'), sg.Button('Salir')]]
 
     layout2 = [
         [sg.Text('Guardar Modelo de Regresión Lineal')],
-        [sg.FileSaveAs(key='fig_save',file_types=(('FARLOPA', '.farlopa'),))]
-    ]
+        [sg.FileSaveAs(key='fig_save',file_types=(('FARLOPA', '.farlopa'),))]]
 
     
     # Crear ventana menu
@@ -152,9 +186,6 @@ def interface(dfs:dict):
             window = sg.Window('Aplicación de Regresión', [[sg.Text('Esta merda esta sin facer')]])
         if event == 'Cargar modelo':
             window = sg.Window('Aplicación de Regresión', [[sg.Text('Esta merda esta sin facer')]])
-
-
-
 
     while True:
         event, values = window.read()
@@ -250,9 +281,7 @@ def interface(dfs:dict):
             window = sg.Window('Aplicación de Regresión', layout2)
 
 
-    
-
-# Cerrar la ventana de la interfaz gráfica al salir
+    # Cerrar la ventana de la interfaz gráfica al salir
     window.close()
 
 
@@ -284,142 +313,6 @@ if event == 'Submit':
         window['-FILE_CONTENT-'].update(file_content[scroll_value:scroll_value + 50])
 
 '''
-#DE AQUI PARA ABAIXO NON SE USA (é do codigo anterior sin interfaz)
-def cargar_csv(file_name):
-    df = pd.read_csv(file_name, sep = ',')
-    df_numeric = df.select_dtypes(include=[np.number])
-    #print(df)
-    print(df_numeric)
-    return(df_numeric)    
-
-def cargar_excel(file_name):
-    df = pd.read_excel(file_name)
-    df_numeric = df.select_dtypes(include=[np.number])
-    #print(df)
-    print(df_numeric)
-    return(df_numeric)
-# Función para cargar una base de datos
-def cargar_basededatos(file_name):
-    conexion = sqlite3.connect(file_name)
-    cursor = conexion.cursor()
-
-    consulta_sql = 'SELECT * FROM california_housing_dataset'
-    cursor.execute(consulta_sql)
-    resultados = cursor.fetchall()
-
-    nombres_columnas = [descripcion[0] for descripcion in cursor.description]
-
-    df = pd.DataFrame(resultados, columns=nombres_columnas)
-
-
-    df_numeric = df.select_dtypes(include=[np.number])
-    print(df_numeric)
-
-    conexion.close()
-    return(df_numeric)
-
-# Función que contiene todas las posibles opciones del programa 
-def menu1(dfs):
-    
-    print("""- · MENU DE OPCIONES · -
-            -> 1: Cargar datos csv
-            -> 2: Cargar datos excel
-            -> 3: Cargar base de datos
-            -> 4: Mostrar archivos cargados
-            -> 5: Hacer regresion lineal
-            -> 6: Cargar modelo
-            -> 0: Salir del programa""")
-    option = int(input(": "))
-
-    if option == 1:
-        file_name = str(input("file_name: "))
-        os.system('clear')
-        df = cargar_csv(file_name)
-        dfs[file_name] = df
-        menu1(dfs)
-        
-    if option == 2:
-        file_name = str(input("file_name: "))
-        os.system('clear')
-        df = cargar_excel(file_name)
-        dfs[file_name] = df
-        menu1(dfs)
-
-    if option == 3:
-        file_name = str(input("file_name: "))
-        os.system('clear')
-        df = cargar_basededatos(file_name)
-        dfs[file_name] = df
-        menu1(dfs)
-    
-    if option == 4:
-        for i in dfs.keys():
-            print(i)
-
-        menu1(dfs)
-
-    if option == 5:
-        print("Seleciona el archivo: ")
-        for i in dfs.keys():
-            print(i)
-        nombre_archivo = str(input(": "))
-        df = dfs[nombre_archivo]
-        print('\nColumnas a selecionnar:\n ')
-        columnas = []
-        for i in range(len(df.columns)):
-            columna = df.columns[i]
-            columnas.append(columna)
-            print("->", i, ":", columna)
-
-        op_columna1 = int(input("\nSelecciona la variable predictora: "))
-        op_columna2= int(input("\nSelecciona la variable a predecir: "))
-
-        X = df[columnas[op_columna1]]
-        Y = df[columnas[op_columna2]]
-
-        print("\n Variable X:")
-        print("\n\t",df.columns[op_columna1])
-        print(X)
-        print("\n Variable Y:")
-        print("\n\t",df.columns[op_columna2])
-        print(Y)
-
-        X = df.iloc[:, op_columna1]
-        Y = df.iloc[:, op_columna2 ]
-
-        X_train, X_test, y_train, y_test = train_test_split(X,
-                                                            Y,
-                                                            test_size=0.2,
-                                                            random_state=1234,
-                                                            shuffle=True)
-        X_train = sm.add_constant(X_train, prepend=True)
-        modelo = sm.OLS(endog=y_train, exog=X_train,)
-        modelo = modelo.fit()
-        print("\n", modelo.summary())
-        
-        print("¿Desea guardar el modelo? (s/n): ")
-        op = str(input(": "))
-
-        if op == 's':
-            nombre_modelo = str(input("Nombre modelo: "))
-            modelo.save(nombre_modelo+'.chantada')
-
-        [sg.Text('Resultado de la Regresión Lineal:', size=(30, 1), key='ResultadoRegresion')]
-        
-        menu1(dfs)
-
-    if option == 6:
-        nombre_modelo = str(input("Introduzca el nombre del modelo a cagar: "))
-        modelo1 = sm.load(nombre_modelo+'.chantada')
-        print("\n", modelo1.summary())
-
-        menu1(dfs)
-
-
-
-        
-
-
 
 if __name__ == '__main__':    
     dfs = {}

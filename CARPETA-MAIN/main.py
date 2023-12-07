437e057fef458a1a068e12cf647d4486d271aecd
ivansio <i.paris@udc.es>
2023-12-07 19:21:45 +0100

    comentei alguns import do main

--- CARPETA-MAIN/main.py
+++ CARPETA-MAIN/main.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import statsmodels.api as sm
-import statsmodels.formula.api as smf
+#import statsmodels.formula.api as smf
 import sqlite3
 import PySimpleGUI as sg
-from sklearn.linear_model import LinearRegression
+#from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
+#from sklearn.metrics import mean_squared_error
 import regression
 
 

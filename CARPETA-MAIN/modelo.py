643da908f013062112a1bfe372569c0a3ea9e0d3
ivan2k4 <126463137+ivan2k4@users.noreply.github.com>
2023-12-15 15:57:05 +0100

    final

--- CARPETA-MAIN/modelo.py
+++ CARPETA-MAIN/modelo.py
@@ -3,26 +3,26 @@ import statsmodels.api as sm
 import pickle
 
 
-def cargar_modelo(pickle_file_name):
+def load_model(pickle_file_name):
     with open(pickle_file_name, "rb") as f:
-        modelo = pickle.load(f)
-    return modelo
+        model = pickle.load(f)
+    return model
 
 
-class Modelo():
+class Model():
     def __init__(self, x_name, y_name,x ,y) -> None:
         self.x_name = x_name
         self.y_name = y_name
         self.x = x
         self.y = y
-        self.modelo = self.make_modelo()
+        self.model = self.make_model()
 
-    def make_modelo(self):
+    def make_model(self):
         x_train, x_test, y_train, y_test = train_test_split(self.x,self.y,test_size=0.2,random_state=1234, shuffle=True)
         x_train = sm.add_constant(x_train)
-        modelo = sm.OLS(endog=y_train, exog=x_train)
-        modelo = modelo.fit()
-        return modelo
+        model = sm.OLS(end_og=y_train, execute_og=x_train)
+        model = model.fit()
+        return model
     
     def get_x_data(self):
         return self.x
@@ -36,10 +36,10 @@ class Modelo():
     def get_y_name(self):
         return self.y_name
     
-    def guardar(self, file_name:str):
+    def save_model(self, file_name:str):
         with open(file_name+'.pickle', 'wb') as f:
             pickle.dump(self, f)
 
-    def get_modelo(self):
-        return self.modelo
+    def get_model(self):
+        return self.model
 

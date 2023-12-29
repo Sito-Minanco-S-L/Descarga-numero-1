158638455ce36b1858e6dcde903facbbe6ebd704
ivansio <i.paris@udc.es>
2023-12-30 00:03:03 +0100

    testing + algunha modificación

---
+++ CARPETA-MAIN/test_modelo.py
@@ -0,0 +1,60 @@
+import modelo
+from modelo import Modelo
+import pytest
+import os
+
+def test_load_model():
+    
+    directorio = 'c:\****\****\******\*****'#ruta del directorio donde se encuentran los archivos
+    archivo1 = 'modelo1.flp.pickle'
+    ruta = os.path.join(directorio, archivo1)
+    assert os.path.isfile(ruta) and modelo.load_model(archivo1) != None
+    
+    archivo2 = 'modelo2.flp.pickle'
+    ruta = os.path.join(directorio, archivo2)
+    assert os.path.isfile(ruta) and modelo.load_model(archivo2) != None
+
+
+
+@pytest.mark.parametrize(
+    "dirigida, directora, x, y",
+
+    [
+        ("dirigida", "directora", [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+        ("dirigida", "directora", [1.2, 3.5, 4.6, 7.99, 0], [1.2, 3.5, 4.6, 7.99, 0]),
+        ("dirigida", "directora", [1.2, 0], [1.2, 0.9]),
+        ("dirigida", "directora", [1], [1])
+
+    ]
+)
+
+def test_make_model(dirigida, directora, x, y):
+    modelo_ = Modelo(dirigida, directora, x, y)
+    model = modelo_.make_model()
+    assert model != None
+    
+#con este test llegamos a que es necesario meter una condición para cuando 
+#las variables seleccionadas contengan menos de 2 valores.
+
+
+modelo1 = Modelo("dirigida", "directora", [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+modelo2 = Modelo("dirigida", "directora", [1.2, 3.5, 4.6, 7.99, 0], [1.2, 3.5, 4.6, 7.99, 0]) 
+modelo3 = Modelo("dirigida", "directora", [1.2, 0], [1.2, 0])
+
+
+@pytest.mark.parametrize(
+    "model, x",
+
+    [
+        (modelo1, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+        (modelo2, [1.2, 3.5, 4.6, 7.99, 0]),
+        (modelo3, [1.2, 0]),
+
+    ]
+)
+
+def test_make_prediction(model, x):
+    
+    model = model.make_model()
+    assert modelo.make_prediction(model, x) is not None
+#testing de las predicciones
---
+++ CARPETA-MAIN/test_regression.py
@@ -0,0 +1,28 @@
+import regression
+import pytest
+
+
+def test_convert_text_to_image_multi():
+    text = 'holaholahola*__123'
+    assert regression.convert_text_to_image(text).endswith('.png')
+
+    text = ''
+    assert regression.convert_text_to_image(text).endswith('.png')
+
+def test_interpret_r_squared():
+    result = regression.interpret_r_squared(2)
+    assert type(result) == tuple
+    result = regression.interpret_r_squared(0.8)
+    assert type(result) == tuple
+    result = regression.interpret_r_squared(0.9)
+    assert type(result) == tuple
+    result = regression.interpret_r_squared(0.5)
+    assert type(result) == tuple
+    result = regression.interpret_r_squared(0.3)
+    assert type(result) == tuple
+    result = regression.interpret_r_squared(0)
+    assert type(result) == tuple
+    
+
+
+
